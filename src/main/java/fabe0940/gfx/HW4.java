fa28a0ea95f571af14b9cc4b96c21e77cd5822cd
Mason Fabel <fabe0940@vandals.uidaho.edu>
2016-04-04 11:02:18 -0700

    Commented main program

--- src/main/java/fabe0940/gfx/HW4.java
+++ src/main/java/fabe0940/gfx/HW4.java
@@ -6,7 +6,7 @@ import fabe0940.gfx.core.*;
 import fabe0940.gfx.math.*;
 
 public class HW4 {
-	private static final int MODE = 0;
+	private static final int MODE = 1;
 
 	public static void main(String[] args) {
 		Viewport view;
@@ -18,7 +18,9 @@ public class HW4 {
 		win = null;
 		world = null;
 
+		/* Select which World to render */
 		switch (MODE) {
+			/* Graph */
 			case 1:
 				view = new ScalingViewport(
 					new Point2(0, 700), new Point2(700, 700),
@@ -28,6 +30,7 @@ public class HW4 {
 					new Point3(0, 0, 0), new Point3(-15, 30, 0), 20);
 				world = new Graph(view, win);
 				break;
+			/* Rubik's Cube */
 			case 2:
 				view = new ScalingViewport(
 					new Point2(0, 700), new Point2(700, 700),
@@ -36,6 +39,7 @@ public class HW4 {
 					new Point3(0, 0, 0), new Point3(-30, 30, 0), 5);
 				world = new Rubik(view, win);
 				break;
+			/* 20x20x20 Rubik's Cubes */
 			case 3:
 				view = new ScalingViewport(
 					new Point2(0, 700), new Point2(700, 700),
@@ -44,6 +48,7 @@ public class HW4 {
 					new Point3(100, 100, 100), new Point3(-30, 30, 0), 250);
 				world = new Rubik20(view, win);
 				break;
+			/* 3D Block Letters */
 			case 4:
 				view = new ScalingViewport(
 					new Point2(0, 700), new Point2(700, 700),
@@ -52,6 +57,7 @@ public class HW4 {
 					new Point3(33, 3.5, -15), new Point3(-30, 30, 0), 100);
 				world = new Name(view, win);
 				break;
+			/* Recognizer */
 			case 5:
 				view = new ScalingViewport(
 					new Point2(0, 700), new Point2(700, 700),
@@ -60,6 +66,7 @@ public class HW4 {
 					new Point3(15, 15, 1.5), new Point3(15, -30, 0), 30);
 				world = new Recognizer(view, win);
 				break;
+			/* Default to unit cube */
 			default:
 				view = new Viewport(new Point2(0, 700), new Point2(700, 700));
 				win = new Window(
@@ -67,8 +74,10 @@ public class HW4 {
 				world = new Cube(view, win);
 		}
 
+		/* Launch a JFrame with the requested World */
 		frame = new Frame(new Point2(200, 100), new Point2(700, 700), world);
 
+		/* Idle - the JFrame will handle rendering calls */
 		while (true);
 	}
 }
